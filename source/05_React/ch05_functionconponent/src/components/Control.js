049fdb5c3e70019baa25a0bc29157a28052a7761
Dsilv118 <dlaehddms9@naver.com>
2023-02-03 17:44:35 +0900

    React-III

---
+++ source/05_React/ch05_functionconponent/src/components/Control.js
@@ -0,0 +1,30 @@
+import './Control.css';
+
+const Control = props => {
+    return (
+        <ul className="Control">
+            <li>
+                <button onClick={()=>{
+                    props.onChangeMode('create');
+                }}>
+                    create
+                </button>
+            </li>
+            <li>
+                <button onClick={()=>{
+                    props.onChangeMode('update');
+                }}>
+                    update
+                </button>
+            </li>
+            <li>
+                <button onClick={()=>{
+                    props.onChangeMode('delete');
+                }}>
+                    delete
+                </button>
+            </li>
+        </ul>
+    );
+};
+export default Control;
