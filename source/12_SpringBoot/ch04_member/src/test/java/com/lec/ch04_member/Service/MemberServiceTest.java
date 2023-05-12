b5f92385b64ec3ed6b1bfe70c7c7ba346c931ef5
Dsilv118 <dlaehddms9@naver.com>
2023-05-12 13:28:34 +0900

    Spring Boot (2)

---
+++ source/12_SpringBoot/ch04_member/src/test/java/com/lec/ch04_member/Service/MemberServiceTest.java
@@ -0,0 +1,28 @@
+package com.lec.ch04_member.Service;
+
+import com.lec.ch04_member.model.Member;
+import com.lec.ch04_member.repository.MemberRepositoryImpl;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService(new MemberRepositoryImpl());
+
+    @BeforeEach
+    void beforeEach() {
+        memberService.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        Member member = new Member("홍길동", "010-9999-9999");
+        int saveId = memberService.join(member);
+        assertThat(saveId).isEqualTo(member.getNo());
+        Member result = memberService.findOne(member.getNo());
+        assertThat(result).isEqualTo(member);
+    }
+}
